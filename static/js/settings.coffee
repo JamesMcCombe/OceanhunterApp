b0001a39f87ea413dd879672a416e4b2e5735bcb
Shijun Liu <lsj5031@outlook.com>
2014-09-04 15:18:24 +1200

    add settings and script tag

---
+++ static/js/settings.coffee
@@ -0,0 +1,4 @@
+settings = 
+	APP_ID: 1492664544305345
+	DOMAIN_NAME: "oceanhunter.node.co.nz"
+	
---
+++ static/js/settings.js
@@ -0,0 +1,6 @@
+var settings;
+
+settings = {
+  APP_ID: 1492664544305345,
+  DOMAIN_NAME: "oceanhunter.node.co.nz"
+};
