ffc9571d6c723feae7c608b0a6c177f9af64f98f
Li Chuangbo <im@chuangbo.li>
2014-09-05 17:28:13 +1200

    add species

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models as m
 
-# Register your models here.
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'k')
+
+admin.site.register(m.Species, SpeciesAdmin)
--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Species(models.Model):
+    class Meta:
+        verbose_name_plural = 'Species'
+
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to="species")
+    k = models.IntegerField(help_text='Weight of calculating points')
