dfbe971f266cefa7cdea1681e05be56fb868dda7
Li Chuangbo <im@chuangbo.li>
2014-09-05 12:33:30 +1200

    add trailing slash

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,8 +6,8 @@ APP_ROOT = path(dirname(abspath(__file__)))
 APP_NAME = APP_ROOT.name
 
 urlpatterns = patterns('%s.views' % APP_NAME,
-                       url(r'^signup$', 'signup', name='signup'),
-                       url(r'^login$', 'login', name='login'),
-                       url(r'^logout$', 'logout', name='logout'),
-                       url(r'^fbuser$', 'fbuser', name='fbuser'),
+                       url(r'^signup/$', 'signup', name='signup'),
+                       url(r'^login/$', 'login', name='login'),
+                       url(r'^logout/$', 'logout', name='logout'),
+                       url(r'^fbuser/$', 'fbuser', name='fbuser'),
                        )
--- apps/main/urls.py
+++ apps/main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('main.views',
     url(r'^$', 'home', name='home'),
-    url(r'^go$', 'go', name='go'),
+    url(r'^go/$', 'go', name='go'),
+    url(r'^invite/$', 'invite', name='invite'),
 )
 
