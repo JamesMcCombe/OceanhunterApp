29bf9d635f522212a15274f7b427af8181dff934
Li Chuangbo <im@chuangbo.li>
2014-09-08 12:48:45 +1200

    remove unnecessary js

--- static/js/settings.js
+++
@@ -1,9 +0,0 @@
-var settings;
-
-settings = {
-  APP_ID: 1492664544305345,
-  DOMAIN_NAME: "http://oceanhunter.node.co.nz",
-  LOGIIN_CALLBACK_URL: "/fbcb"
-};
-
-settings.LOGIN_URL = "https://www.facebook.com/v2.0/dialog/oauth?client_id=" + settings.APP_ID + "&scope=publish_actions,user_friends&redirect_uri=" + settings.DOMAIN_NAME + settings.LOGIIN_CALLBACK_URL;
