89d3ad0dd8cce3592bcaedbddf73297813a6fc15
ubuntu <developers@node.co.nz>
2014-09-10 16:10:43 +1200

    change product.py

--- settings/product.py
+++ settings/product.py
@@ -3,10 +3,10 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 # this is the default backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-WORKON_HOME = path('/var/www')
+WORKON_HOME = path('/var/www/django')
 VENV_ROOT  = WORKON_HOME/PROJ_NAME
 
-WWW_ROOT  = PROJ_ROOT.parent/'public-www'
+WWW_ROOT  = VENT_ROOT/'public-www'
 STATIC_ROOT = WWW_ROOT/'static'
 MEDIA_ROOT  = WWW_ROOT/'media'
 CACHE_ROOT  = WWW_ROOT/'cache'
