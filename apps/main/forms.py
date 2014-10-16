1b4c4fa6af4b59b1bf668018776a5c4eaff7933c
Li Chuangbo <im@chuangbo.li>
2014-10-17 11:30:03 +1300

    render filter clear buttons from form

--- apps/main/forms.py
+++ apps/main/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.encoding import force_text
 from . import models as m
 from accounts import models as am
 
@@ -40,3 +41,44 @@ class FilterForm(forms.Form):
     team = forms.ChoiceField(widget=forms.RadioSelect, choices=TEAM_CHOICES)
     age = forms.ChoiceField(widget=forms.RadioSelect, choices=AGE_CHOICES)
     gender = forms.ChoiceField(widget=forms.RadioSelect, choices=am.GENDER_CHOICES)
+
+    def filters(self):
+        """For leader board. Get non empty field and get the label of the choice
+        """
+        filters = []
+        for field in self:
+            choice_value = field.value()
+            choices = field.field.choices
+            if choice_value:
+                choice_label = get_choice_label(choices, choice_value)
+                field.choice_label = choice_label
+                filters.append(field)
+        return filters
+
+
+def get_choice_label(choices, value):
+    """ Get label from django form choices
+    >>> get_choice_label((('1', 'Northland'), ('2', 'Auckland')), '1')
+    'Auckland'
+    >>> get_choice_label(((1, 'Northland'), (2, 'Auckland')), '1')
+    'Auckland'
+    >>> get_choice_label([('', 'Region'), ('North Island', ((1, 'Northland'), (2, 'Auckland'))), ('South Island', (('3', 'XXX')))], '1')
+    'Auckland'
+    """
+    for choice_value, choice_label in choices:
+        if choice_value is None:
+            choice_value = ''
+        choice_value = force_text(choice_value)
+        if isinstance(choice_label, (list, tuple)):
+            sub_choices = choice_label
+            choice_label = get_choice_label(sub_choices, value)
+            if choice_label:
+                return choice_label
+        else:
+            if value == choice_value:
+                return choice_label
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
