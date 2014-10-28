654b8bc0ab81c4549aaa0554274a73513bcdf056
guoqiao <guoqiao@gmail.com>
2014-10-29 10:51:24 +1300

    add password change and reset urls and templates

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -12,3 +12,13 @@ urlpatterns = patterns('%s.views' % APP_NAME,
                        url(r'^logout/$', 'logout', name='logout'),
                        url(r'^fbuser/$', 'fbuser', name='fbuser'),
                        )
+
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^password/change/$', 'password_change', name="password_change"),
+    url(r'^password/change/done/$', 'password_change_done', name="password_change_done"),
+    url(r'^password/reset/$', 'password_reset', name="password_reset"),
+    url(r'^password/reset/done/$', 'password_reset_done', name='password_reset_done'),
+    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        'password_reset_confirm', name="password_reset_confirm"),
+    url(r'^password/done/$', 'password_reset_complete', name="password_reset_complete")
+)
