8b29715d3c1d24c14c04650253f2b999fe134eee
Li Chuangbo <im@chuangbo.li>
2014-11-03 11:19:18 +1300

    fix accept '%Y-%m-%d' as signup dob format

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -36,7 +36,7 @@ class SignupForm(forms.ModelForm):
     gender = forms.ChoiceField(widget=forms.RadioSelect, choices=m.GENDER_CHOICES)
     # area = forms.ChoiceField(widget=forms.RadioSelect, choices=m.AREA_CHOICES)
     city = forms.ChoiceField(label="City", choices=m.CITY_CHOICES)
-    dob = forms.DateField(label="Date of Birth", input_formats=['%d/%m/%Y', '%d/%m/%y'])
+    dob = forms.DateField(label="Date of Birth", input_formats=['%d/%m/%Y', '%d/%m/%y', '%Y-%m-%d'])
     #postcode = forms.CharField(label="Postcode")
     #phone = forms.CharField(label="Phone Number")
 
