1fb4969aa95f7315fc422a26a483a1997f06774a
guoqiao <guoqiao@gmail.com>
2014-11-05 10:04:46 +1300

    show slug for page admin

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models as m
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'create', 'content')
+    list_display = ('title', 'slug', 'user', 'create', 'content')
 
 admin.site.register(m.Page, PageAdmin)
