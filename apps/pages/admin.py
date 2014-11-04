96cc39bf0a24510fa1ecd0ebfba031f383ee33b9
guoqiao <guoqiao@gmail.com>
2014-11-05 10:54:08 +1300

    improve page model and admin

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -4,4 +4,9 @@ from . import models as m
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'user', 'create', 'content')
 
+    def save_model(self, request, obj, form, change):
+        u = request.user
+        obj.user = u
+        obj.save()
+
 admin.site.register(m.Page, PageAdmin)
