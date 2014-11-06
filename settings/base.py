73c96e2ccfc140b8f7c56d5dd30e82c980f72814
guoqiao <guoqiao@gmail.com>
2014-11-06 14:31:58 +1300

    change ADMINS and MANAGERS

--- settings/base.py
+++ settings/base.py
@@ -15,7 +15,14 @@ SERVER_NAME = 'example.com'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@w@4gg7^dy9z3(r%n0b&*rr)4cf0-b$=hpsyx5qo0r4l3b=i$8'
 
-MANAGERS = (('Mike', 'mike@oceanhunter.co.nz'), ('Node', 'node@node.co.nz'))
+ADMINS = (
+    ('Node Developers', 'developers@node.co.nz'), 
+)
+
+MANAGERS = (
+    ('Mike', 'mike@oceanhunter.co.nz'), 
+    ('James', 'james@node.co.nz'),
+)
 
 ALLOWED_HOSTS = ['*']
 
