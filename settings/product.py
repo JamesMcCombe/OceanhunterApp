674ee9b0fe93b2624b91e1729525fc1de051bff3
Ilian Iliev <ilian@ilian.io>
2016-06-24 10:00:54 +0200

    Commentign unused settings

--- settings/product.py
+++ settings/product.py
@@ -1,27 +1,29 @@
 from base import *
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-# this is the default backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-WORKON_HOME = path('/var/www/django')
-VENV_ROOT  = WORKON_HOME/PROJ_NAME
 
-WWW_ROOT  = VENV_ROOT/'public-www'
-STATIC_ROOT = WWW_ROOT/'static'
-MEDIA_ROOT  = WWW_ROOT/'media'
-CACHE_ROOT  = WWW_ROOT/'cache'
+# # this is the default backend
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# WORKON_HOME = path('/var/www/django')
+# VENV_ROOT  = WORKON_HOME/PROJ_NAME
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': CACHE_ROOT,
-    },
-    'staticfiles': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION': 'staticfiles-filehashes'
-    }
-}
-
-SITE_URL = 'https://oceanhunter.node.co.nz'
+# WWW_ROOT  = VENV_ROOT/'public-www'
+# STATIC_ROOT = WWW_ROOT/'static'
+# MEDIA_ROOT  = WWW_ROOT/'media'
+# CACHE_ROOT  = WWW_ROOT/'cache'
+#
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+#
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+#         'LOCATION': CACHE_ROOT,
+#     },
+#     'staticfiles': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'staticfiles-filehashes'
+#     }
+# }
+#
+# SITE_URL = 'https://oceanhunter.node.co.nz'
